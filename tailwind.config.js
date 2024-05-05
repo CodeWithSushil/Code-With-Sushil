07b22a9f27a4e3195e85802749942563fa3b80ce
CodeWithSushil <sushilkumbhar77@gmail.com>
2024-05-05 09:53:12 +0530

    init project

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./resources/**/*.{html,js,php,twig}",
+    "./public/*.{html,js,php,twig}"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
